1ff8640da187ff066445da8db323f7ae2cb7b219
arnav0103 <66205884+arnav0103@users.noreply.github.com>
2022-01-05 13:17:14 +0530

    a

--- Tool/__init__.py
+++ Tool/__init__.py
@@ -10,7 +10,7 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xcdnsqpnigrsrf:0736e45fd73a9240f73a52de250d0628d1b3de3fd648acb7fdd7022cdbecac45@ec2-3-224-251-59.compute-1.amazonaws.com:5432/dacbmprjqvsrst'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jsoggfhxqxzisa:2b78d9f278c306075010f3d524a503f59e8191e145f4522238bde00d4f638b26@ec2-54-83-157-174.compute-1.amazonaws.com:5432/d5p553tfqa95h7'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'mysecret'
 db = SQLAlchemy(app)
